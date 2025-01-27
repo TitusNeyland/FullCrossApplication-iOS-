b572697221015cf7429fc7e10c10afc4b9ee6b02
Titus Neyland <titusaneyland@gmail.com>
2025-01-26 18:21:16 -0600

    Initial Commit

---
+++ FullCrossApplication(iOS)/FullCrossApplication_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  FullCrossApplication_iOS_App.swift
+//  FullCrossApplication(iOS)
+//
+//  Created by Titus Neyland on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FullCrossApplication_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FullCrossApplication(iOS)Tests/FullCrossApplication_iOS_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FullCrossApplication_iOS_Tests.swift
+//  FullCrossApplication(iOS)Tests
+//
+//  Created by Titus Neyland on 1/26/25.
+//
+
+import Testing
+@testable import FullCrossApplication_iOS_
+
+struct FullCrossApplication_iOS_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
